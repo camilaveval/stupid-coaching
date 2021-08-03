3f147bccf1ec55a113dcf6c35acab79655c4317d
camila vera <camilapazvera@gmail.com>
2021-08-03 17:33:54 -0400

    rails new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,14 @@
 class QuestionsController < ApplicationController
+    def ask
+    end
+
+    def answer
+      if params[:ask] == "I am going to work"
+        @respuesta = "Great!"
+      elsif params[:ask].end_with?("?")
+        @respuesta = "Silly question, get dressed and go to work!"
+      else params[:ask] != ""
+        @respuesta = "I don't care, get dressed and go to work!"
+      end
+    end
 end
